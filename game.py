d78d2a7f0bfb9bfd60cc3305665b99337350561b
Apoorva Somani <apoorvasomani1994@gmail.com>
2020-03-06 08:03:55 +0530

    Update game.py

--- game.py
+++ game.py
@@ -24,24 +24,26 @@ NEGATIVE_SCORE_MAP = {
 	HARD: 10
 }
 
+START_TEXT = '''\n\n********************************** GAMBLE **********************************
+Just guess what will be the sum of the numbers on all the dices and win.
+Its harder to win and very easy to loose!!
+
+Try as many times you want and try to improve your score\n\n'''
+
 
 def get_name():
-	special_print('Enter Player Name - ')
-	PLAYER_NAME = input()
-	PLAYER_NAME.capitalize()
 	
-	initiate_play()
+	global PLAYER_NAME
+
+	print('Enter Player Name - ', end="")
+	
+	PLAYER_NAME = input()
+	PLAYER_NAME = PLAYER_NAME.capitalize()
 
 
 def initiate_play(again=False):
-	play_text = '''
-	Hey %s, do you want to play %s??
-
-	1. Yes
-	2. No
-
-	Enter choice number ...
-	'''
+	
+	play_text = 'Hey %s, do you want to play %s?? \n1. Yes \n2. No \n\nEnter choice number - '
 	
 	again_text = '\b'
 	if again:
@@ -49,34 +51,43 @@ def initiate_play(again=False):
 	
 	play_text = play_text % (PLAYER_NAME, again_text)
 
-	special_print(play_text)
+	print_line()
+	print(play_text, end='')
+
+	choice = input()
 	
-	choice = int(input())
+	try:
+		choice = int(choice)
+	except:
+		print('Incorrect choice!!!')
+		return initiate_play()
 
 	if choice == 1:
 		level_selection()
 	elif choice == 2:
 		initiate_exit()
 	else:
-		special_print('Incorrect Option')
+		print('Incorrect choice!!!')
 		return initiate_play()
 
 
 def level_selection():
-	level_selection_text = '''
-	%s, Choose your level -
+	
+	global PLAYER_LEVEL, POSITIVE_SCORE, NEGATIVE_SCORE
 
-	1. Easy [1 - DIE]
-	2. Medium [2 - DIE]
-	3. Hard [3 - DIE]
+	level_selection_text = '%s, Choose your level - \n1. Easy [1 - DICE] - +5/-2 \n2. Medium [2 - DICES] - +10/-5 \n3. Hard [3 - DICES] - +20/-10 \n\nEnter choice number - '
 
-	Enter choice number ...
-	'''
+	print_line() 
+	print(level_selection_text % PLAYER_NAME, end='')
 
-	special_print(level_selection_text % PLAYER_NAME)
-
-	choice = int(input())
-
+	try:
+		choice = int(input())
+	except:
+		print('Incorrect choice!!!')
+		return level_selection()
+	
+	print_line()
+	
 	level_map = {
 		1: EASY,
 		2: MEDIUM,
@@ -86,7 +97,7 @@ def level_selection():
 	if level_map.get(choice):
 		PLAYER_LEVEL = level_map[choice]
 	else:
-		special_print('Incorrect choice!!!')
+		print('Incorrect choice!!!')
 		return level_selection()
 
 	POSITIVE_SCORE = POSITIVE_SCORE_MAP[PLAYER_LEVEL]
@@ -97,6 +108,8 @@ def level_selection():
 
 def start_game():
 
+	global PLAYER_SCORE
+
 	die_sum = 0
 
 	number_of_die_throws = {
@@ -105,42 +118,50 @@ def start_game():
 		HARD: 3
 	}
 
-	special_print('Guess the sum of trials - ')
+	print('Guess the sum - ')
 	try:
 		player_guess = int(input())
+		print_line()
 	except:
-		special_print('Some error has occurred!!')
+		print('Some error has occurred!!')
 		return start_game()
 
-	special_print('Lets roll ...')
+	print('Lets roll ...')
 
-	for count in xrange(number_of_die_throws[PLAYER_LEVEL]):
-		
-		for seconds in xrange(randint(3,8)):
-			special_print('......')
-			time.sleep(seconds)
-			die_number = random.randint(1,6)
-			print('The number on die %s is %s' % (count, die_number))
+	for count in range(number_of_die_throws[PLAYER_LEVEL]):
+		for seconds in range(randint(3,5)):
+			sleep(seconds)
+		die_number = randint(1,6)
+		print('The number on die %s is %s' % (count + 1, die_number))
 
-			die_sum += die_number
+		die_sum += die_number
 
+	sleep(5)
 	if die_sum == player_guess:
 		PLAYER_SCORE += POSITIVE_SCORE
 	else:
 		PLAYER_SCORE -= NEGATIVE_SCORE
 
+	print_line()
+	print('Your current score is %d' % PLAYER_SCORE)
+
 	initiate_play(again=True)
 
 
 def initiate_exit():
-	special_print('%s\'s final score is %s.' % (PLAYER_NAME, PLAYER_SCORE))
-	special_print('Goodbye!!!!')
+	print_line()
+	sleep(2)
+	print('%s\'s final score is %d.' % (PLAYER_NAME, PLAYER_SCORE))
+	print_line()
+	sleep(3)
+	print('Goodbye!!!!')
+	print_line()
 
 
-def special_print(text):
-
-	for letter in text:
-		print(letter, end='')
-		sleep(0.10)
+def print_line():
+	print('-' * 50)
 	
+
+print(START_TEXT)
 get_name()
+initiate_play()
